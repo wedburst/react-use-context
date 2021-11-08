d84189cbef4a022ba6eb16ddc1a5355a0b032bf4
wedburst <jhonrawed@gmail.com>
2021-11-07 23:00:12 -0500

    mejoras en ruta de react-router-dom v6

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,27 +1,27 @@
-import React from 'react';
+import React from "react";
 import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    Navigate
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
 } from "react-router-dom";
 
 // Componentes
-import { HomeScreen } from '../components/HomeScreen';
-import { AboutScreen } from '../components/AboutScreen';
-import { LoginScreen } from '../components/LoginScreen';
-import { NavBar } from '../components/ui/NavBar';
+import { HomeScreen } from "../components/HomeScreen";
+import { AboutScreen } from "../components/AboutScreen";
+import { LoginScreen } from "../components/LoginScreen";
+import { NavBar } from "../components/ui/NavBar";
 
 export const AppRouter = () => {
-    return(
-        <Router>
-            <NavBar/>
-            <Routes>
-                <Route path="/" element={<HomeScreen/>} />
-                <Route path="/about" element={<AboutScreen/>} />
-                <Route path="/login" element={<LoginScreen/>} />
-
-            </Routes>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/about" element={<AboutScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+};
